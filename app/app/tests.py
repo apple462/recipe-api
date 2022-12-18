3be991d69f74623b11b1efbb6d489a066c21fcdc
Laksh <63868266+apple462@users.noreply.github.com>
2022-12-18 16:06:22 +0530

    Linting fixed + wait for db done

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,13 +6,14 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-  """Test the calc module"""
+    """Test the calc module"""
 
-  def test_add_numbers(self):
-    """Test adding numbers"""
-    self.assertEqual(calc.add(3,8), 11)
+    def test_add_numbers(self):
+        """Test adding numbers"""
+        self.assertEqual(calc.add(3, 8), 11)
 
-  def test_subtract_numbers(self):
-    """Test subtracting numbers"""
-    self.assertEqual(calc.subtract(5,11), 6)
+    def test_subtract_numbers(self):
+        """Test subtracting numbers"""
+        self.assertEqual(calc.subtract(5, 11), 6)
